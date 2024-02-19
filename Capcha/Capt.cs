be5cc8df41e57eeace39f3051e08f6663cbfad48
SeregaMerkul <154590602+SeregaMerkul@users.noreply.github.com>
2024-02-19 09:42:46 +0300

    капча.dll

---
+++ Capcha/Capt.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capcha
+{
+    public class Capt
+    {
+
+         public static string text = String.Empty;
+
+
+        public static Bitmap CreateImage(int Width, int Height)
+        {
+        
+        
+        Random rnd = new Random();
+
+            Bitmap result = new Bitmap(Width, Height);
+
+            int Xpos = rnd.Next(0, Width - 50);
+            int Ypos = rnd.Next(15, Height - 15);
+
+            Brush[] colors =
+            {
+                Brushes.Black,
+                Brushes.Red,
+                Brushes.RoyalBlue,
+                Brushes.Green
+            };
+
+            Graphics g = Graphics.FromImage((Image)result);
+            g.Clear(Color.Gray);
+
+            text = String.Empty;
+            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+            for (int i = 0; i<5; i++)
+                text += ALF[rnd.Next(ALF.Length)];
+            for (int i = 0; i < 5; ++i)
+                text += ALF[rnd.Next(ALF.Length)];
+            g.DrawString(text,
+             new Font("Arial", 15),
+             colors[rnd.Next(colors.Length)],
+             new PointF(Xpos, Ypos));
+            g.DrawLine(Pens.Black,
+           new Point(0, 0),
+           new Point(Width - 1, Height - 1));
+            g.DrawLine(Pens.Black,
+                       new Point(0, Height - 1),
+                       new Point(Width - 1, 0));
+            for (int i = 0; i<Width; i++)
+                for (int j = 0; j < Height; j++)
+                    if (rnd.Next() % 20 == 0)
+                        result.SetPixel(i, j, Color.White);
+            return result;
+        }
+
+    }
+    }
+
